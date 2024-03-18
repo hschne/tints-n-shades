46259f12a910ba39a99a78089adad41c21e64f50
hschne <hans.schnedlitz@gmail.com>
2024-03-18 14:11:44 +0100

    Add multi output

--- site/script.js
+++ site/script.js
@@ -3,28 +3,47 @@ const response = await fetch("./ruby-web.wasm");
 const module = await WebAssembly.compileStreaming(response);
 const { vm } = await DefaultRubyVM(module);
 
-function generatePalette() {
-  const color = document.querySelector("#color").value;
-  const name = document.querySelector("#color").value;
-
+const generate = (color, name, format, output) => {
   const result = vm.eval(`
   require "/bundle/setup"
   require "js"
   require "tns"
 
   color = "${color}"
-  rgb = TNS::Color::RGB.from_hex(color)
   name = "${name}"
-  color_format = "hex"
-  output = "tailwind"
+  color_format = "${format}"
+  output = "${output}"
+  rgb = TNS::Color::RGB.from_hex(color)
   palette = TNS::Palette.new(rgb).to(color_format)
   output = TNS::Output.from_argument(output, name)
   output.format(palette)
 `);
+  return result.toString();
+};
 
-  document.querySelector("#output").innerHTML = result;
-}
+const onClick = () => {
+  const color = document.querySelector("#color").value;
+  const name = document.querySelector("#name").value;
+  const format = document.querySelector("#format").value;
 
-document.querySelector("#button").addEventListener("click", (_event) => {
-  generatePalette();
-});
+  document.querySelector("#css").innerHTML = generate(
+    color,
+    name,
+    format,
+    "css",
+  );
+  document.querySelector("#sass").innerHTML = generate(
+    color,
+    name,
+    format,
+    "sass",
+  );
+  document.querySelector("#tailwind").innerHTML = generate(
+    color,
+    name,
+    format,
+    "tailwind",
+  );
+};
+
+document.querySelector("#button").addEventListener("click", onClick);
